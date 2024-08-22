46b109e4688703d9d0b0486a022d2e70ea81f22d
ignacio poletti <ip@discoverit.store>
2024-08-21 22:36:41 -0300

    FIX | Modified id constant at route.ts

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -5,7 +5,7 @@ import clientPromise from "@/utils/mongodb";
 export async function GET(req: NextRequest) {
   try {
     const url = new URL(req.url);
-    const id = url.pathname.split("/").pop();
+    const id = await url.pathname.split("/").pop();
 
     const client = await clientPromise;
     const db = client.db("test");
