7f58274594acbee1314772b2c54edee5201b28dd
ignacio poletti <ip@discoverit.store>
2024-09-04 19:15:56 -0300

    FEATURE | Agregado botón de main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { Header } from "./components";
 import { ProductType } from "@/app/types";
 import ProductCard from "./components/card";
-import { Spinner } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
   const [products, setProducts] = useState<ProductType[]>([]);
@@ -44,10 +45,13 @@ export default function Home() {
       <div className="flex justify-center text-center pt-12 text-2xl font-semibold">
         <h2>TODO LO NUEVO.</h2>
       </div>
-      <div className="flex flex-wrap justify-center gap-6 p-10">
+      <div className="flex flex-wrap justify-center gap-6 p-10 relative z-10">
         {products.map((product) => (
           <ProductCard key={product._id} data={product} />
         ))}
+        <div className="flex justify-center bg-black py-4 w-full">
+            <Button as={Link} href="pages/products" color="primary" className="bg-transparent border-2 border-sky-700 hover:bg-sky-700 hover:drop-shadow-[0_15px_20px_rgba(29,78,216,0.25)] duration-300 text-sky-700 hover:text-white">Ver todo</Button>
+        </div>
       </div>
     </main>
   );
